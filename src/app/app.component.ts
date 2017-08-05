f1c04e7d3939673d1f4f0a0d38ea530f1af4a743
Arjun Raju Pillai <revarjunr@gmail.com>
2017-08-05 23:47:37 +0530

    Seperate app component template to its own file.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,24 +4,7 @@ import { Hero } from './hero/hero';
 
 @Component({
   selector: 'my-app',
-  // templateUrl: './app.component.html',
-  template: `
-    <div class="content">
-      <h1>{{title}}</h1>
-      <h2>My favorite hero is {{hero}}</h2>
-      <p>Heroes:</p>
-      <ul>
-        <li *ngFor="let hero of heroes">
-          <h4>{{ hero.name }} details</h4>
-          <div class="details">
-            <p><label>id: </label>{{ hero.id }}</p>
-            <p><label>name: </label>{{ hero.name }}</p>
-          </div>
-        </li>
-      </ul>
-      <p *ngIf="heroes.length > 3">There's quite a few heroes here.</p>
-    </div>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.styl']
 })
 export class AppComponent {
